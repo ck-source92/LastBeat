ebeaa91e4f7eb81a060d2c7db04a85b699b59b0b
Dwi Candra Kirana <dwicandra920@gmail.com>
2023-06-27 08:39:18 +0700

    adding ads

--- Assets/Script/MenuManager/AudioMainMenu.cs
+++ Assets/Script/MenuManager/AudioMainMenu.cs
@@ -1,17 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Advertisements;
 public class AudioMainMenu : MonoBehaviour
 {
-    private void Awake()
+    AudioSource m_AudioSource;
+    void Awake()
     {
+        m_AudioSource = GetComponent<AudioSource>();
         int numberScenePersist = FindObjectsOfType<AudioMainMenu>().Length;
         if (numberScenePersist > 1)
         {
             Destroy(gameObject);
         }
         else { DontDestroyOnLoad(gameObject); }
+    }
+    void Update()
+    {
+        Debug.Log(Advertisement.isShowing + "hello");
+        m_AudioSource.mute = Advertisement.isShowing;
     }
     public void ResetAudioMainMenu()
     {
